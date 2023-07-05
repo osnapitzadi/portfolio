a430d7d31a2d542d2c3474134e07fdd94397a2eb
osnapitzadi <osnapitzadi@gmail.com>
2023-07-05 18:20:03 -0400

    no animation

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 function Projects() {
   return (
-    <div>Projects</div>
+    <div className='projects m-3 align-self-end ms-auto'>
+        <div className='d-flex flex-column m-3'>
+            <div className='project'>NASA API</div>
+            <div className='project'>Weather API</div>
+            <div className='project'>React Todo</div>
+        </div>
+    </div>
   )
 }
 
