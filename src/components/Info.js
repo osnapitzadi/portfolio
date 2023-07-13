cc01fcc0eb4d99082964608856d51008aba566ad
osnapitzadi <osnapitzadi@gmail.com>
2023-07-13 03:33:55 -0400

    framer cond animation

--- src/components/Info.js
+++ src/components/Info.js
@@ -2,9 +2,13 @@ import React from 'react'
 
 function Info() {
   return (
-    <div className={`m-3 align-self-end ms-auto info col-sm-12 col-md-6 col-lg-3`}>Born in 1996 in Almaty, Qazaqstan. I firmly believe that the extent of one's potential remains immeasurable unless they are willing to embrace failure and persevere through countless attempts.
-    </div>
-  )
+            <div>Born in 1996 in Almaty, Qazaqstan. 
+                I firmly believe that the extent of 
+                one's potential remains immeasurable 
+                unless they are willing to embrace 
+                failure and persevere through countless attempts.
+            </div>
+        )
 }
 
 export default Info
