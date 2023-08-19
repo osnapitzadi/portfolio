22d14ea97ea7828b0a6e978381c5864bb9214e23
osnapitzadi <osnapitzadi@gmail.com>
2023-08-19 10:38:49 -0400

    added CMS websites

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,27 +3,48 @@ import React from 'react'
 function Projects() {
   const projects = [
     {
-      id: 1,
-      title: "NASA API",
-      url: ""
+      id: 7,
+      title: "Travel Agency",
+      url: "https://travelclub.kz/"
     },
     {
-      id: 2,
-      title: "Weather API",
-      url: ""
+      id: 5,
+      title: "Jewelry Store",
+      url: "https://atush-studio.ru/"
+    },
+    {
+      id: 6,
+      title: "Event Agency",
+      url: "https://www.moscowstars.ru/"
+    }, 
+    {
+      id: 4,
+      title: "Productivity App",
+      url: "https://productivity-app-prod.web.app"
     },
     {
       id: 3,
       title: "React Todo",
-      url: ""
-    }
+      url: "https://react-todo-adil.web.app/"
+    },
+    {
+      id: 1,
+      title: "NASA API",
+      url: "https://nasa-apod-api-adil.web.app/"
+    },
+    {
+      id: 2,
+      title: "Vanilla JS Store (First project)",
+      url: "https://projectlevel1adil.web.app/"
+    },
+    
   ];
   return (
-        <div>
+        <div className='scrollable'>
           {projects.map(project => (
-          <div className='project' key={project.id}>
+          <div className='menu-item'>• <a className='project' key={project.id} href={project.url} target='_blank'>
             {project.title}
-          </div>
+          </a></div>
           ))}
         </div>
   )
